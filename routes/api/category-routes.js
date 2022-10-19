0a53d8b04bd77f1f57b501e96110523c9783a0ca
saucymj <mekhi.banks@ymail.com>
2022-10-18 23:45:00 -0500

    fixed api put routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,9 +38,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const newCategory = await Category.create({
-      product_id: req.body.product_id,
-    });
+    const newCategory = await Category.create(req.body);
     res.status(200).json(newCategory);
   } catch (err) {
     res.status(400).json(err);
